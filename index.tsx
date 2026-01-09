00ea7b6e073560879a6ffa226707cb5a14716237
pringgosatmoko <58687046+pringgosatmoko@users.noreply.github.com>
2026-01-09 16:29:41 +0700

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,16 +1,5 @@
+// index.tsx (DISABLED)
+// File ini sengaja dimatikan
+// Supaya tidak bentrok dengan index.html
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export {}
