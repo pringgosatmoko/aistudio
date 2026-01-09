8f2756db508e7a9597e9528fdb4ef7f982110972
Pringgo Satmoko <pringgosatmoko@gmail.com>
2026-01-09 20:57:52 +0700

    Fix Git conflict in index.tsx

--- index.tsx
+++ index.tsx
@@ -1,24 +1,9 @@
-<<<<<<< HEAD
-// index.tsx (DISABLED)
-// File ini sengaja dimatikan
-// Supaya tidak bentrok dengan index.html
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-export {}
-=======
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
->>>>>>> b52a159 (Initial commit SATMOKO Creative Studio AI)
